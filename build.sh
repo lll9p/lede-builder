66cc7abbc68219c530c46e903aaa2384d1a849e0
lll9p <lll9p.china@gmail.com>
2020-02-23 18:40:22 +0800

    valid yaml

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 BUILD_PATH="/home/build"
+echo 'Clone lede.'
+echo ''
+if [[ ! -d ${BUILD_PATH}/build/lll9p/lede-builder/lede ]]; then
+    git clone https://github.com/coolsnowwolf/lede
+fi
+
+echo 'Clone serverchan.'
+echo ''
+if [[ ! -d ${BUILD_PATH}/build/lll9p/lede-builder/luci-app-serverchan ]]; then
+    git clone https://github.com/tty228/luci-app-serverchan
+fi
+
 echo 'Start build script.'
 echo ''
 
